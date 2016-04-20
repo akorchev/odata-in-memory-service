cc96f294ad6a90abae1664f120535b1209f23814
unknown <akorchev@gmail.com>
2016-04-20 07:19:35 -0700

    Allow certain HTTP headers.

--- odata-service/Global.asax.cs
+++ odata-service/Global.asax.cs
@@ -27,7 +27,9 @@ namespace ODataService
             if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
             {
                 HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "POST, PUT, PATCH, DELETE");
-                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "*");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Credentials", "true");
+
                 HttpContext.Current.Response.End();
             }
         }
